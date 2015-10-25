58d48e8856729af59827225a092017772a4dac03
Markus Linnala <Markus.Linnala@gmail.com>
2015-10-25 17:35:47 +0200

    working

--- foo.pl
+++ foo.pl
@@ -7,24 +7,30 @@ use Data::Dumper;
  
 my $e = Search::Elasticsearch->new();
 
+sub index_bulk {
+  my $bulk = shift;
+  my $r = $e->bulk(
+    index => 'my_index',
+    type => 'my_type',
+    body => $bulk,
+  );
+  #print Dumper($r);
+  @{$bulk} = ();
+}
+
 $| = 1;
+
 my @bulk;
+my $id = 1;
 print "0";
 for (1..10000) {
-  print "\r$_";
+  print "\r$_     ";
   push @bulk,
-    { index => { _id => $_ } },
+    { index => { _id => $id } },
     { message => sprintf('Prefix something %f suffix other', rand(1000)), },
   ;
-  if (@bulk == 100) {
-    my $r = $e->bulk(
-      index => 'my_index',
-      type => 'my_type',
-      body => \@bulk,
-    );
-    print Dumper($r);
-    @bulk = ();
-  }
+  $id ++;
+  index_bulk(\@bulk) if (@bulk == 100);
   if (0) {
     $e->index(
       index => 'my_index',
@@ -36,13 +42,15 @@ for (1..10000) {
     );
   }
 }
-if (@bulk) {
-  my $r = $e->bulk(
-    index => 'my_index',
-    type => 'my_type',
-    body => \@bulk,
-  );
-  print Dumper($r);
-  @bulk = ();
+index_bulk(\@bulk) if (@bulk);
+for (1..10000) {
+  print "\r$_     ";
+  push @bulk,
+    { index => { _id => $id } },
+    { message => sprintf('ERROR: Why we get this, now %d %f', int($_/5), rand(1000)), },
+  ;
+  $id ++;
+  index_bulk(\@bulk) if (@bulk == 100);
 }
+index_bulk(\@bulk) if (@bulk);
 print "\n";
--- search.pl
+++ search.pl
@@ -1,16 +1,85 @@
 #!/usr/bin/perl -Tw
 
 use Search::Elasticsearch;
+use Data::Dumper;
+use JSON::XS;
+use MIME::Base64;
  
 # Connect to localhost:9200:
  
 my $e = Search::Elasticsearch->new();
+my $json = JSON::XS->new->pretty(1)->canonical(1)->ascii(1)->space_before(0);
 
+if (0) {
 my $results = $e->search(
   index => 'my_index',
   body  => {
     query => {
-      match => { },
+      term => { _type => 'my_type', },
+    },
+    fields => [qw'message minhash_value'],
+  },
+);
+}
+
+my $scroll = $e->scroll_helper(
+  index       => 'my_index',
+  search_type => 'scan',          # important!
+  size        => 500,
+  body        => {
+    query => {
+      term => { _type => 'my_type', },
+    },
+    fields => [qw'message minhash_value'],
+  },
+);
+
+my %h;
+my $maxlen = 128;
+my @bfreq;
+
+sub handle_doc {
+  local $_ = shift;
+
+  for my $v (@{$_->{fields}->{minhash_value}}) {
+    my $bits = decode_base64($v);
+    #print '>'.$bits."<\n";
+    #$h{ $_->{_id} } = $bits;
+    if (0) {
+      my $bs = unpack('b*', $bits);
+      my $len = length($bs);
+      $maxlen = $len if (not defined $maxlen or $len > $maxlen);
+    }
+    for (my $i = 0; $i < $maxlen; $i++) {
+      my $b = vec($bits, $i, 1);
+      push @{$bfreq[$i][$b]}, $_->{_id};
+      $bfreq{$i.' '.$b} ++;
     }
   }
-);
+}
+
+while (my $doc = $scroll->next) {
+  #print $json->encode($doc);
+  handle_doc($doc);
+}
+
+#print Dumper($results);
+#print $json->encode($results);
+#my %h;
+#my $maxlen = 128;
+#die unless (defined $results->{hits}->{hits});
+#for (@{$results->{hits}->{hits}}) {
+#  #print $json->encode($_);
+#}
+#print "maxlen: $maxlen\n";
+
+for my $k (sort { $bfreq{$a} <=> $bfreq{$b} } keys %bfreq) {
+  my ($i, $c) = split(/ /, $k, 2);
+  my %cnt;
+  for my $id (@{$bfreq[$i][$c]}) {
+    $cnt{ int(($id-0)/10000) } ++;
+  }
+  printf("%s %d %d %d\n", $k, $bfreq{$k}, $cnt{0}, $cnt{1});
+}
+
+#print $json->encode(\@bfreq);
